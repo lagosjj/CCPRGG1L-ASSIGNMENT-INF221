7bcf2dac9c9d5f2c25ce5910d1fd8f78687f62d2
lagosjj <111472838+lagosjj@users.noreply.github.com>
2022-10-04 13:21:42 +0800

    Create Assignment2.java

---
+++ Assignment2.java
@@ -0,0 +1,34 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+            //For LOOP
+        for (int firstName = 5; firstName != 0; firstName--) {
+            System.out.println("John Jed");
+             //BREAK
+            if ( firstName == 2){
+              System.out.println("Time out");
+              break;
+          }
+        }
+            //DO WHILE
+           int Surname = 5;  
+        do {
+            System.out.println("Lagos");
+            Surname--;
+        } while (Surname != 0);
+        
+            // WHILE LOOP
+        int nickName = 5;
+        while (nickName !=0){
+            
+            nickName--;
+            //CONTINUE
+            if (nickName ==3){
+                continue;}
+            
+            System.out.println("JJ");
+            
+            
+            
+            }
+        }
+    }
